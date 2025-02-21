47d313ae9550394999873210236b7069a501409e
KushK461 <kevin.kamae@strathmore.edu>
2025-02-21 15:50:37 +0300

    Added secure password input

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-name = input("Enter name: ")
-enc = input("Pass is: ")
+import getpass
 
-#name and enc conditions
+name = input("Enter name: ")
+enc = getpass.getpass("Pass is: ")  # Hides input for better security
+
+# Name and enc conditions
 if not name.isalpha():
     print("Error! only letters allowed in name.")
 
 elif not enc.isdigit():
     print("Password should only have numbers!")
 
-#password length condition
+# Password length condition
 elif len(enc) < 8:
     print("Password should be at least 8 digits long!")
 
-# If both are valid,output
+# If both are valid, output
 else:
     print("Name is:", name, "Password is:", enc)
